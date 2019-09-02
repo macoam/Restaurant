15ebdb5fdf2affd1b961d6f4f15439abed8b5c2d
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-02 12:38:00 -0700

    Agregar archivos de proyecto.

---
+++ Restaurant/Platillos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    class Platillos
+    {
+        public string Identificador { get; set; }
+        public string Nombre { get; set; }
+        public string Descripción { get; set; }
+        private double _precio;
+        public double Precio
+        {
+            get
+            {
+                return _precio;
+            }
+
+            set
+            {
+                if (value >= 50)
+                {
+                    _precio = value;
+                }
+
+                else
+                {
+                    throw new Exception("La comida vale más de $50");
+                }
+            }
+        }
+    }
+}
